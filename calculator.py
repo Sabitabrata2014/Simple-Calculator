73643a06e2917305befeade1a748f4cd1b1a1531
sabitabrata2014 <sabitabrata2014@gmail.com>
2024-04-02 15:36:30 +0530

    Made the calculator menu based

--- calculator.py
+++ calculator.py
@@ -1,25 +1,31 @@
 from operators import *
 
-#User Input
-a=float(input("Enter the first number: "))
-b=float(input("Enter the second number: "))
-op=input("Enter the operator: ")
+while(True):
+	#User Input
+	a=float(input("Enter the first number: "))
+	b=float(input("Enter the second number: "))
+	op=input("Enter the operator: ")
 
-#Conditional Call
-if(op=="+"):
-  sum(a,b)
+	#Conditional Call
+	if(op=="+"):
+  		sum(a,b)
 
-elif(op=="-"):
-  dif(a,b)
+	elif(op=="-"):
+  		dif(a,b)
 
-elif(op=="*"):
-  prod(a,b)
+	elif(op=="*"):
+  		prod(a,b)
 
-elif(op=="/"):
-  quo(a,b)
+	elif(op=="/"):
+  		quo(a,b)
 
-elif(op=="%"):
-  rem(a,b)
+	elif(op=="%"):
+  		rem(a,b)
 
-else:
-  print("Invalid Operator")
+	else:
+  		print("Invalid Operator")
+	repeat=input('do you want to try again(yes/no): ')
+  	if(repeat.lower()=='no'):
+    		break
+  	else:
+    		pass
