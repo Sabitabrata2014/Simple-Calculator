fce35ec56d4429e7289ba9d7e5ff384cdccf2105
sabitabrata2014 <sabitabrata2014@gmail.com>
2024-04-02 15:06:02 +0530

    the calculator program is split into two parts

---
+++ operators.py
@@ -0,0 +1,15 @@
+#Operators
+def sum(a,b):
+  print("The sum is",a+b)
+
+def dif(a,b):
+  print("The difference is",a-b)
+
+def prod(a,b):
+  print("The product is",a*b)
+
+def quo(a,b):
+  print("The quotient is",a/b)
+
+def rem(a,b):
+  print("The remainder is",a%b)
